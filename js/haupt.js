2cf369d9ec6fa031b8dc31fc2ce7be9c07fa589d
Noah Horlacher <kuromarumusic@gmail.com>
2023-08-04 11:16:15 +0200

    Added automatic translation

--- js/haupt.js
+++ js/haupt.js
@@ -1,7 +1,29 @@
 import { verdreheSatz } from "./wortverdreher.js"
 
 const verdrehKnopf = document.querySelector('button')
+const automatischÜberprüfKasten = document.querySelector('#automatischÜberprüfKasten')
 const austuh = document.querySelector('p')
 const eintuh = document.querySelector('textarea')
 
-verdrehKnopf.addEventListener('click', ()=>austuh.innerText = verdreheSatz(eintuh.value))
+let automatischesVerdrehen = automatischÜberprüfKasten.checked = true
+
+function austuhAktualisieren(){
+    console.log('aktualisiert')
+    austuh.innerText = verdreheSatz(eintuh.value)
+}
+verdrehKnopf.addEventListener('click', austuhAktualisieren)
+
+automatischÜberprüfKasten.addEventListener('change', e => {
+    automatischesVerdrehen = automatischÜberprüfKasten.checked
+    automatischVerdrehenUmschalten()
+})
+
+function automatischVerdrehenUmschalten(){
+    if(automatischesVerdrehen){
+        eintuh.addEventListener('input', austuhAktualisieren)
+
+        austuhAktualisieren()
+    } else {
+        eintuh.removeEventListener('input', austuhAktualisieren)
+    }
+}
